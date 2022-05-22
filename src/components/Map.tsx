6a36c756db94bd0ba429428e1fbc406dd9b3158e
James Ward <james.jward@hotmail.com>
2022-05-22 01:02:03 -0300

    Cleaned up repo. Basic map functionality is working

---
+++ src/components/Map.tsx
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+import {
+    MapContainer, TileLayer, Marker, Popup
+} from 'react-leaflet'
+
+import 'leaflet/dist/leaflet.css';
+import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.webpack.css';
+import 'leaflet-defaulticon-compatibility';
+
+// Map Layer Urls
+const mapLayerUrls = {
+    satelite: 'https://api.maptiler.com/maps/hybrid/256/{z}/{x}/{y}@2x.jpg?key=Anh4h0YsAYHxUN84Fy1a',
+    pastelle: 'https://api.maptiler.com/maps/pastel/256/{z}/{x}/{y}@2x.png?key=Anh4h0YsAYHxUN84Fy1a',
+    street: 'https://api.maptiler.com/maps/streets/256/{z}/{x}/{y}@2x.png?key=Anh4h0YsAYHxUN84Fy1a'
+}
+
+export const Map = () => {
+
+    const [center, setCenter] = useState<number[]>([53.350140, -6.266155]);
+
+    return (
+        <MapContainer center={center} zoom={13} scrollWheelZoom={false} style={{ height: '100vh', width: '80vw', borderRadius: '10px' }} >
+            <TileLayer
+                url={mapLayerUrls.street}
+                attribution='<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
+            />
+            <Marker position={[53.350140, -6.266155]}>
+                <Popup>
+                    A pretty CSS3 popup. <br /> Easily customizable.
+                </Popup>
+            </Marker>
+        </MapContainer >
+    )
+}
+
+export default Map
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,56 +1,31 @@
-import {
-  Link as ChakraLink,
-  Text,
-  Code,
-  List,
-  ListIcon,
-  ListItem,
-} from '@chakra-ui/react'
-import { CheckCircleIcon, LinkIcon } from '@chakra-ui/icons'
-
+import dynamic from 'next/dynamic'
+import { Box, Text } from '@chakra-ui/react'
 import { Hero } from '../components/Hero'
 import { Container } from '../components/Container'
 import { Main } from '../components/Main'
 import { DarkModeSwitch } from '../components/DarkModeSwitch'
-import { CTA } from '../components/CTA'
 import { Footer } from '../components/Footer'
 
-const Index = () => (
-  <Container height="100vh">
-    <Hero />
-    <Main>
-      <Text color="text">
-        Example repository of <Code>Next.js</Code> + <Code>chakra-ui</Code> +{' '}
-        <Code>TypeScript</Code>.
-      </Text>
-
-      <List spacing={3} my={0} color="text">
-        <ListItem>
-          <ListIcon as={CheckCircleIcon} color="green.500" />
-          <ChakraLink
-            isExternal
-            href="https://chakra-ui.com"
-            flexGrow={1}
-            mr={2}
-          >
-            Chakra UI <LinkIcon />
-          </ChakraLink>
-        </ListItem>
-        <ListItem>
-          <ListIcon as={CheckCircleIcon} color="green.500" />
-          <ChakraLink isExternal href="https://nextjs.org" flexGrow={1} mr={2}>
-            Next.js <LinkIcon />
-          </ChakraLink>
-        </ListItem>
-      </List>
-    </Main>
-
-    <DarkModeSwitch />
-    <Footer>
-      <Text>Next ❤️ Chakra</Text>
-    </Footer>
-    <CTA />
-  </Container>
+const Map = dynamic(
+  () => import('../components/Map'),
+  { ssr: false }
 )
+
+const Index = () => {
+
+  return (
+    <Container height="100vh">
+      <Hero />
+      <Main>
+        <Map />
+      </Main>
+
+      <DarkModeSwitch />
+      <Footer>
+        <Text>James ❤️ Aila</Text>
+      </Footer>
+    </Container>
+  )
+}
 
 export default Index
